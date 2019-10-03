4d720febb8c13e0272ded59239587153792c2eb7
Rommel Terrence Juanillo <juanillo.terrence@gmail.com>
2019-10-03 17:33:49 +0800

    initial setup for ope

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='openabis-nfiq',
+    version='0.0.1',
+    packages=['openabis_nfiq'],
+    url='https://github.com/newlogic42/openabis-nfiq',
+    license='Apache License 2.0',
+    author='newlogic42',
+    author_email='',
+    description='OpenAbis\' integration plugin for NFIQ'
+)
