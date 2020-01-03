98583ccc672887e492ff5333db6326820bcc2fa3
Jeremi Joslin <jeremi@newlogic.io>
2020-01-03 15:43:40 +0800

    Fix the build to include NFIQ

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from distutils.core import setup
 
 setup(
     name='openabis-nfiq',
-    version='0.0.1',
+    version='0.0.2',
     packages=['openabis_nfiq'],
     url='https://github.com/newlogic42/openabis-nfiq',
     license='Apache License 2.0',
     author='newlogic42',
     author_email='',
     description='OpenAbis\' integration plugin for NFIQ',
+    include_package_data=True,
     install_requires=[
         'pillow==6.2.1'
     ]
